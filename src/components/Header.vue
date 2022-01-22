10a71ff59c32814dca81e59ce764eba2cdc1d555
Simon Lambin <slambin@artfx.fr>
2022-01-22 13:22:04 +0100

    Create the Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      Emilie Fatous
+    </div>
+  </v-app-bar>
+</template>
+
+
+<script>
+  export default {
+    name: 'Header',
+
+    data: () => ({
+    }),
+  }
+</script>
